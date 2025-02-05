ec9500620275f82c531e2f39ec8288a5ed427e4f
weviavek <we.viavek@gmail.com>
2025-02-06 02:36:25 +0530

    1.0.3

--- lib/file_scan.dart
+++ lib/file_scan.dart
@@ -1,11 +1,25 @@
 import 'dart:io';
-
 import 'package:flutter/services.dart';
 
+/// A utility class to trigger file system updates on Android devices.
+///
+/// This class interacts with the Android `MediaScannerConnection` to ensure
+/// that newly added or modified files are recognized by media-related apps
+/// like Gallery and File Manager.
 class FileScan {
+  /// Adds a file or directory to the Android file system, making it visible
+  /// to media applications.
+  ///
+  /// The [filePath] parameter should be the absolute path of the file or
+  /// directory you want to add.
+  ///
+  /// Throws an exception if the operation fails.
   static Future<void> addFile({required String filePath}) async {
     try {
-      if (Platform.isAndroid) await const MethodChannel('com.filescan/refresh').invokeMethod('refresh', {'path': filePath});
+      if (Platform.isAndroid) {
+        await const MethodChannel('com.filescan/refresh')
+            .invokeMethod('refresh', {'path': filePath});
+      }
     } catch (e) {
       rethrow;
     }
